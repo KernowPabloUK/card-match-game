577e763ae47da82dc478e73394aa52f5cdcbd7b1
KernowPabloUK <PaulMorganUK1@outlook.com>
2025-06-16 15:27:37 +0100

    ADD: Implement initial game layout and start game functionality

--- script.js
+++ script.js
@@ -1,11 +1,48 @@
-// start game (once start game button is clicked)
+document.addEventListener("DOMContentLoaded", function () {
+    // start game (once start game button is clicked)
+    let gameBoardContainer = document.querySelector("#gameBoardContainer");
+    document
+        .querySelector("#buttonStartGame")
+        .addEventListener("click", startGame);
+    let gameBoardHeight = 4;
+    let gameBoardWidth = 4;
+    let gameCardsAnimals = ["cow", "cow", "duck", "duck", "pig", "pig", "lamb", "lamb", "chicken", "chicken", "llama", "llama", "dog", "dog", "cat", "cat"]
 
-// create a matrix array (4x4 - 8unique // 6x6 18unique // 8x8 32unique)
+    function startGame() {
+        console.log("start1 button clicked");
 
-// randomise allocation of unique card pairs into matrix
+        // ensure gameBcontainer exists
+        if (!gameBoardContainer) {
+            console.error("gameBoardContainer element not found!");
+            return;
+        }
 
-// on click, flip card, remain unless reclicked
+        // clear previous game board if exists
+        gameBoardContainer.innerHTML = "";
 
-// on second click, flip card, if matching, remain visible
+        // create a matrices array (4x4 - 8unique)
+        for (let row = 0; row < gameBoardHeight; row++) {
+            const rowDiv = document.createElement("div");
+            rowDiv.classList.add("row");
 
-// once all cards are flipped correctly - game over message - play again - change difficulty
+            for (let column = 0; column < gameBoardWidth; column++) {
+                const cellDiv = document.createElement("div");
+                cellDiv.classList.add("cell");
+                rowDiv.appendChild(cellDiv);
+            }
+            gameBoardContainer.appendChild(rowDiv);
+        }
+    // randomise allocation of unique card pairs into matrix
+
+    }
+
+
+    // on click, flip card, remain unless reclicked
+
+    // on second click, flip card, if matching, remain visible
+
+    // once all cards are flipped correctly - game over message - play again - change difficulty
+});
+
+// TODO POST 6x6 18unique // TODO POST 8x8 32unique
+// TODO different themes
