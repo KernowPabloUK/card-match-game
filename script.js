00653721d967905fab47d6bac552fb12e8c99b07
KernowPabloUK <PaulMorganUK1@outlook.com>
2025-06-16 14:24:45 +0100

    ADD: Psuedo code and initial file creation

---
+++ script.js
@@ -0,0 +1,11 @@
+// start game (once start game button is clicked)
+
+// create a matrix array (4x4 - 8unique // 6x6 18unique // 8x8 32unique)
+
+// randomise allocation of unique card pairs into matrix
+
+// on click, flip card, remain unless reclicked
+
+// on second click, flip card, if matching, remain visible
+
+// once all cards are flipped correctly - game over message - play again - change difficulty
